f3cd230e0ac2dd392f4bc2196b6c3257313a3464
Penny Borelli <Penny.Borelli@supersport.com>
2024-07-27 11:45:06 +0200

    Refactor: Extract a query object.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -14,7 +14,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   if (error) return null;
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+      <MenuButton left={2} as={Button} rightIcon={<BsChevronDown />}>
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
